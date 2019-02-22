22d75fe4c9844d776a7ea765433f2e794e0ef7d0
Christophe HAMERLING <chamerling@linagora.com>
2019-02-22 19:11:53 +0100

    Widget is a card with props and slots

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    <slot name="body"></slot>
-  </div>
+  <v-card>
+    <v-card-title>
+      <v-icon large left>{{icon}}</v-icon>
+      <span class="title font-weight-light ml-2">{{title}}</span>
+    </v-card-title>
+    <v-card-text>
+      <slot name="body"></slot>
+    </v-card-text>
+  </v-card>
 </template>
-
 
 <script>
 export default {
-  name: "widget"
+  name: "widget",
+  props: {
+    title: String,
+    icon: String
+  }
 }
 </script>
