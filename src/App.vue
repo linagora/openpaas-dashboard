abe69013ee117772a33987cd62c8e9bc1bf7cf93
Christophe HAMERLING <chamerling@linagora.com>
2019-03-18 12:12:40 +0100

    Change BG color on login

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="app">
-    <div v-if="$auth.ready()">
+    <div v-if="$auth.ready()" :style="{ backgroundColor: backgroundColor }" id="app-ready">
       <v-toolbar clipped-left app fixed color="blue" v-if="$auth.check()">
         <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
           <img class="hidden-sm-and-down" id="header-logo" src="@/assets/logo.svg"/>
@@ -16,7 +16,7 @@
         </v-container>
       </v-content>
     </div>
-    <div v-else>
+    <div v-else id="progress">
       <v-progress-circular indeterminate :size="50" color="primary"></v-progress-circular>
     </div>
     <snackbar/>
@@ -27,10 +27,16 @@
 import { mapGetters, mapState } from "vuex";
 import UserMenu from "@/components/UserMenu.vue";
 import Snackbar from "@/components/Snackbar.vue";
+import colors from "vuetify/es5/util/colors";
 
 export default {
   data: () => ({}),
   computed: {
+    backgroundColor() {
+      if (!this.$auth.check()) {
+        return colors.blue.base;
+      }
+    },
     ...mapState({
       connectedUser: "user"
     }),
@@ -52,11 +58,24 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  #app #app-ready
+    height: 100vh;
+
+  #app-ready
+    transition .4s
+
   #header-logo
     height: 35px;
     width: 150px;
 
   .container {
     padding: 0;
+  }
+
+  #progress {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 </style>
