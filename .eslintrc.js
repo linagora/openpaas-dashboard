0162178992750fb4a08a75a1c21049a07b032bfb
Christophe HAMERLING <chamerling@linagora.com>
2019-05-16 10:10:01 +0200

    #38 Add eslint for vue-18n plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,21 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ["plugin:vue-i18n/recommended", "plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "max-len": ["error", { code: 120 }],
-    "prettier/prettier": [process.env.NODE_ENV === "production" ? "off" : "error", { printWidth: 120 }]
+    "prettier/prettier": [process.env.NODE_ENV === "production" ? "off" : "error", { printWidth: 120 }],
+    "vue-i18n/no-dynamic-keys": "warn",
+    "vue-i18n/no-unused-keys": ["error", { extensions: [".js", ".vue"] }]
   },
   parserOptions: {
     parser: "babel-eslint"
+  },
+  settings: {
+    "vue-i18n": {
+      localeDir: "./i18n/lang/*.json"
+    }
   }
 };
