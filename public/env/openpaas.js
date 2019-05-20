6d6a1bc24676467c88883c937fe46991a091b2c8
Christophe HAMERLING <chamerling@linagora.com>
2019-05-20 14:53:47 +0200

    #46 Define app settings from window.openpaas

---
+++ public/env/openpaas.js
@@ -0,0 +1,5 @@
+window.openpaas = {
+  // This as to be injected at runtime
+  // For example, when running with docker, mount a volume here and push right values in this file
+  //VUE_APP_OPENPAAS_URL: "https://dev.open-paas.org"
+};
--- src/services/application-settings.js
+++ src/services/application-settings.js
@@ -1,9 +1,7 @@
-import env from "@/config/env";
-
 export const ApplicationSettings = Object.freeze({
   ...{ BASE_URL: "/", VUE_APP_OPENPAAS_URL: "http://localhost:8080" },
   ...process.env,
-  ...env
+  ...window.openpaas
 });
 
 export default ApplicationSettings;
