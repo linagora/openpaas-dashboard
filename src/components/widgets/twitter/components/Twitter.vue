1d9cc931e998b0dcf9f32eacef904162771c7c3f
Christophe HAMERLING <chamerling@linagora.com>
2019-06-17 09:54:36 +0200

    Twitter support

---
+++ src/components/widgets/twitter/components/Twitter.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="github">
+    <v-layout row wrap v-if="lastItems && lastItems.length">
+      <v-flex>
+        <v-list>
+          <template v-for="(item, index) in lastItems">
+            <tweet :item="item" :key="item.id"/>
+            <v-divider v-if="index + 1 < lastItems.length" :key="index"></v-divider>
+          </template>
+        </v-list>
+      </v-flex>
+    </v-layout>
+    <v-container
+      v-else-if="error"
+      pt-0 align-center justify-center align-content-center d-flex :style="{flexDirection: 'column'}"
+    >
+      <v-flex pb-2>
+        <span class="title font-weight-light red--text">{{$t(error)}}</span>
+      </v-flex>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Vue from "vue";
+import Tweet from "./Tweet.vue";
+
+export default {
+  name: "Twitter",
+  mounted() {
+    this.fetch();
+  },
+  data: () => ({
+    delay: 60 * 1000,
+    limit: 10,
+    timeout: null,
+    items: null,
+    error: null
+  }),
+  computed: {
+    lastItems() {
+      return (this.items || []).slice(0, this.limit);
+    },
+    configuration() {
+      const twitterAccount = this.currentUser.accounts.find(
+        account => account.type === "oauth" && account.data && account.data && account.data.provider === "twitter"
+      );
+
+      return twitterAccount && twitterAccount.data;
+    },
+    ...mapGetters({
+      currentUser: "user/getCurrentUser"
+    })
+  },
+  methods: {
+    fetch() {
+      if (!this.configuration) {
+        this.error = "Twitter account is not configured";
+
+        return;
+      }
+
+      this.$emit("loading", true);
+
+      Vue.$openpaas.twitter
+        .getMentions({ account_id: this.configuration.id })
+        .then(items => {
+          this.error && (this.error = null); // avoid tilt while loading after an error by waiting for feed items
+          this.items = (items && items.messages) || [];
+        })
+        .catch(err => {
+          console.log("Error while getting tweets", err);
+          this.error = "Can not load tweets";
+        })
+        .finally(() => this.$emit("loading", false));
+
+      this.timeout = setTimeout(this.fetch, this.delay);
+    },
+    clear() {
+      this.timeout && clearTimeout(this.timeout);
+      this.items = null;
+    }
+  },
+  destroyed() {
+    this.clear();
+  },
+  components: {
+    Tweet
+  }
+};
+</script>
