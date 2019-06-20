05468f052119b0c9c93980d14909a2bfabde2e11
Christophe HAMERLING <chamerling@linagora.com>
2019-06-20 11:55:26 +0200

    #70 Add google news widget

---
+++ src/components/widgets/news/index.js
@@ -0,0 +1,21 @@
+import Feed from "../rss/components/Feed.vue";
+
+const components = {
+  main: { component: Feed }
+};
+
+const settings = {
+  data: {
+    url: "https://news.google.com/rss?hl=en",
+    limit: 8
+  }
+};
+
+export default {
+  title: "Google News",
+  type: "googlenews",
+  icon: "public",
+  description: "Display latest new from Google",
+  components,
+  settings
+};
