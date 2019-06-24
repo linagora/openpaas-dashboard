1a73df725650e2fb369274a46a19d3fd918b3dd5
Christophe HAMERLING <chamerling@linagora.com>
2019-06-24 10:31:31 +0200

    #83 Do not open dashboard on menu click

--- src/components/ui/Sidebar.vue
+++ src/components/ui/Sidebar.vue
@@ -35,7 +35,7 @@
         </v-list-tile-content>
         <v-list-tile-action>
           <v-menu bottom left offset-y close-on-click>
-            <v-btn data-test="dashboard-operations" slot="activator" flat icon ripple>
+            <v-btn data-test="dashboard-operations" slot="activator" flat icon ripple @click.native.prevent>
               <v-icon>more_vert</v-icon>
             </v-btn>
             <v-list>
