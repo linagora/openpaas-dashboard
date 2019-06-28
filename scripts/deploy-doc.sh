c6774f1b3a3b27c4a5546453cb3095ed30aa0baf
Christophe HAMERLING <chamerling@linagora.com>
2019-06-28 12:24:58 +0200

    #101 Set git credentials

--- scripts/deploy-doc.sh
+++ scripts/deploy-doc.sh
@@ -3,16 +3,22 @@
 # abort on errors
 set -e
 
+rev=$(git rev-parse --short HEAD)
+release_message="Updated at ${rev}"
+
 # build
 npm run docs:build
 
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
+git config --global user.name "CI Bot"
+git config --global user.email "bot@linagora.com"
+
 git init
 git add -A
-git commit -m 'deploy'
+git commit --allow-empty -m "$release_message"
 
-git push -f git@github.com:linagora/openpaas-dashboard.git master:gh-pages
+git push -f https://${GITHUB_TOKEN}@github.com/linagora/openpaas-dashboard.git master:gh-pages
 
 cd -
