0f68918925fe36d13dbceb6cf2aff795903d3c47
Christophe HAMERLING <chamerling@linagora.com>
2019-09-05 14:32:38 +0200

    line-through when task is done

--- src/components/widgets/todo/components/Entry.vue
+++ src/components/widgets/todo/components/Entry.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-list-tile :class="{ 'done': todo.done }">
+  <v-list-tile>
     <v-list-tile-action>
       <v-checkbox v-model="done"></v-checkbox>
     </v-list-tile-action>
     <v-list-tile-content>
-      <v-list-tile-title>{{ todo.title }}</v-list-tile-title>
+      <v-list-tile-title class="title" :class="{ 'done' : todo.done }">{{ todo.title }}</v-list-tile-title>
       <v-list-tile-sub-title>{{ todo.created_at | moment("from") }}</v-list-tile-sub-title>
     </v-list-tile-content>
   </v-list-tile>
@@ -36,6 +36,6 @@ export default {
 
 <style lang="stylus" scoped>
 .done {
-  color: red
+  text-decoration: line-through
 }
 </style>
